3b8be7de52191e042e7bf6e0572b0ba668599dc3
NikhilSharma-007 <niks21902@gmail.com>
2025-03-27 17:08:34 +0530

    Update layout and add react query in layout

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,37 @@
+"use client";
 import Footer from "@/app/_components/Footer";
 import Header from "@/app/_components/Header";
 import "@/app/_styles/global.css";
 import { Outfit } from "next/font/google";
+import { usePathname } from "next/navigation";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const outfit = Outfit({
   subsets: ["latin"],
   weights: ["400", "500", "600", "700"],
 });
 
-export const metadata = {
+const metadata = {
   title: "Creative homes",
-  description: "Made by yogesh",
+  description: "Made By Nikhil",
 };
 
 export default function RootLayout({ children }) {
+  const [queryClient] = useState(() => new QueryClient());
+  const pathname = usePathname();
+  const isAdminRoute = pathname?.startsWith("/admin/");
+
   return (
     <html lang="en">
-      <body className={outfit.className} style={{position:"relative"}}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
+      <QueryClientProvider client={queryClient}>
+        <body className={outfit.className} style={{ position: "relative" }}>
+          {!isAdminRoute && <Header />}{" "}
+          <ReactQueryDevtools initialIsOpen={false} /> {children}
+          {!isAdminRoute && <Footer />}{" "}
+        </body>
+      </QueryClientProvider>
     </html>
   );
 }
