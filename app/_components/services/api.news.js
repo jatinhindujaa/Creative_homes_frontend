1dd616951dafecddb38c05c102452c4dd506131b
NikhilSharma-007 <niks21902@gmail.com>
2025-04-23 12:14:39 +0530

    changes

---
+++ app/_components/services/api.news.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { ApiUrl } from "./Api";
+
+export const fetchNews = async () => {
+  const response = await axios.get(`${ApiUrl}/news/get-all`, {
+    withCredentials: true,
+  });
+  return response.data.message; // Adjust based on actual API structure
+};
+
+export const createNews = async (formData) => {
+  const response = await axios.post(`${ApiUrl}/news/create`, formData, {
+    withCredentials: true,
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response.data;
+};
+
+export const updateNews = async ({ id, formData }) => {
+  const response = await axios.post(
+    `${ApiUrl}/news/update?id=${id}`,
+    formData,
+    {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
+  return response.data;
+};
+
+export const deleteNews = async (id) => {
+  const response = await axios.get(`${ApiUrl}/news/delete?id=${id}`, {
+    withCredentials: true,
+  });
+  return response.data;
+};
+
+export const fetchNewsById = async (id) => {
+  const response = await axios.get(`${ApiUrl}/news/get-by-id?_id=${id}`, {
+    withCredentials: true,
+  });
+  return response.data.message;
+};
+
+export const updateImage = async ({ id, formData }) => {
+  const response = await axios.post(
+    `${ApiUrl}/news/update-image?id=${id}`,
+    formData,
+    {
+      withCredentials: true,
+    }
+  );
+  return response.data;
+};
+
+export const updateBanner = async ({ id, formData }) => {
+  const response = await axios.post(
+    `${ApiUrl}/news/update-banner?id=${id}`,
+    formData,
+    {
+      withCredentials: true,
+    }
+  );
+  return response.data;
+};
---
+++ app/admin/news/page.js
@@ -0,0 +1,5 @@
+import News from "@/app/_components/admin/news/News";
+
+export default function Index() {
+  return <News />;
+}
