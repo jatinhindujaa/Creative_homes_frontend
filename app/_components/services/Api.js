dedd20180200e257610a5aa5a410ce3323b17db8
jatin2040947 <jatin@mobyink.com>
2025-05-20 18:26:12 +0530

    c

--- app/_components/services/Api.js
+++ app/_components/services/Api.js
@@ -1,4 +1,4 @@
-export const ApiUrl = "http://localhost:8000/api/v1";
+export const ApiUrl = "https://creative-homes-backend-1.onrender.com";
 
 //http://localhost:8000/api/v1
 // https://creative-homes-backend-1.onrender.com
