8a42a934f58a7c73de4181abc704620e35058af4
jatin2040947 <jatin@mobyink.com>
2025-06-18 17:49:16 +0530

    changee

--- app/_components/services/Api.js
+++ app/_components/services/Api.js
@@ -1,4 +1,4 @@
-export const ApiUrl = "https://appsita.com/api/v1";
+export const ApiUrl = "https://creativehomes.ae/api/v1";
 
 //http://localhost:8000/api/v1
 // https://creative-homes-backend-1.onrender.com/api/v1
